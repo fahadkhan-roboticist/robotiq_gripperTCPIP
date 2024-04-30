dd07fb7f92de1b8b7ef46ad13939dfa1761d8fc8
fahadkhan-roboticist <fahadkhan.nmims@gmail.com>
2024-04-30 12:28:06 +0100

    Gripper control usingTCP IP

---
+++ gripper_hande.py
@@ -0,0 +1,14 @@
+import socket 
+
+HOST = "192.168.0.50"    # The remote host 
+PORT = 30002              # The same port as used by the server 
+PORTG = 63352
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+s.connect((HOST, PORTG))
+s.sendall(b'SET POS 3\n')
+data = s.recv(2**10)
+
+print('Gripper finger position is: ', data)
+
+s.close()
